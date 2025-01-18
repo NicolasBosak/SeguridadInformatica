a41e3a23c2c415a537391efc3d78ddde5cc10144
Nicolas Casamen <nicolascasamen@outlook.com>
2025-01-18 01:36:49 -0500

    Add - Adding styles for the pages.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,7 @@ namespace SeguridadInformatica
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Black.ttf", "Roboto");
                 });
 
 #if DEBUG
